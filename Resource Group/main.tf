293d1fd1f7790c876f08bc5ede79588b1d07ea92
Rishikesh Sharma <srishikesh39@gmai.com>
2025-07-12 15:23:41 +0530

    rG

--- Resource Group/main.tf
+++ Resource Group/main.tf
@@ -3,8 +3,8 @@ resource "azurerm_resource_group" "pks_rg" {
         name = each.value.name
         location = each.value.location
     }
-resource "azurerm_resource_group" "pks_rg2" {
-    for_each = var.pks_rg2 
+resource "azurerm_resource_group" "pks_rg5" {
+    for_each = var.pks_rg5 
         name = each.value.name
         location = each.value.location
     }

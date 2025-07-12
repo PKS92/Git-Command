8fbeb508870495bf29668182a4aa7685c4075572
Prashant <prashant.shukla80@gmail.com>
2025-07-12 14:51:39 +0530

    1st git updated commad

--- Resource Group/main.tf
+++ Resource Group/main.tf
@@ -3,3 +3,8 @@ resource "azurerm_resource_group" "pks_rg" {
         name = each.value.name
         location = each.value.location
     }
+resource "azurerm_resource_group" "pks_rg1" {
+    for_each = var.pks_rg1 
+        name = each.value.name
+        location = each.value.location
+    }
